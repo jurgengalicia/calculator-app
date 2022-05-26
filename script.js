443f6a571a597827185db467ee0c09a43b8d88d8
jurgengalicia <jurgen.galicia@gmail.com>
2022-05-25 21:07:20 -0700

    applied css, added calculator logic

---
+++ script.js
@@ -0,0 +1,26 @@
+
+const result = document.getElementById("resultText");
+const calculation = document.getElementById("calculationText");
+
+// inserts number on click
+function insert(num){
+    calculation.textContent += num;
+    console.log(num);
+}
+
+document.getElementById("clear").addEventListener("click", function(){
+    calculation.textContent = "";
+    result.textContent = 0;
+});
+
+
+document.getElementById("equal").addEventListener("click", function(){
+    var exp = caluclation.textContent;
+    result.textContent = eval(exp);
+    calculation.textContent = result.textContent;
+});
+
+document.getElementById("back").addEventListener("click", funcvtion(){
+    var exp = calculation.textContent;
+    calculation.textContent = exp.substring(0, exp.length - 1);
+});
